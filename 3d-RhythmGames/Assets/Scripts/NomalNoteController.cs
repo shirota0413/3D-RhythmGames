c8f21bb870e5ca3a0e49da77430a278570a7bd73
島袋太智 <e185712@e185712.local>
2020-07-01 22:43:52 +0900

    音楽・譜面の読み込み・ノーツ作成

---
+++ 3d-RhythmGames/Assets/Scripts/NomalNoteController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NomalNoteController : MonoBehaviour {
+    public Transform[] lanePos;
+    int Block;
+
+    float Distance;
+    float During;
+
+
+    float starttime;
+    float now;
+    float duration;
+    float arrivalTime = 0.6f;
+
+    float Vx;
+    float Vy;
+    Vector3 firstScale;
+    Vector3 scale;
+
+    public void setParameter(int block) {
+        Block = block;
+    }
+    
+    public int getBlock(){
+        return Block;
+    }
+    // Start is called before the first frame update
+    void Start() {
+        starttime = Time.time;
+        Vector3 pos = lanePos[getBlock()].transform.position;
+        Vx = pos.x / arrivalTime;
+        Vy = pos.y / arrivalTime;
+        firstScale = this.gameObject.transform.localScale;
+        scale = lanePos[getBlock()].transform.localScale - firstScale;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        now = Time.time;
+        duration = now - starttime;
+        this.gameObject.transform.position = new Vector3(Vx * duration , Vy * duration,0);
+        if (duration > arrivalTime + 0.2f) {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ 3d-RhythmGames/Assets/Scripts/SongController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongController : MonoBehaviour {
+    AudioSource Music;
+    public string ClipPath;
+
+    //Start is called before the first frame update
+    void Start() {
+        Music = this.GetComponent<AudioSource>();
+        Music.clip = (AudioClip)Resources.Load(ClipPath);
+        Music.Play();
+    }
+}
